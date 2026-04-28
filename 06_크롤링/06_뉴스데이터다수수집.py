a09274d118a5264245044691890c612e9a75e2a9
tjoeun-260225 <tjoeun.jr5@gmail.com>
2026-04-28 12:37:06 +0900

    웹크롤링 requests _ BeautifulSoup

--- 06_크롤링/06_뉴스데이터다수수집.py
+++ 06_크롤링/06_뉴스데이터다수수집.py
@@ -5,6 +5,39 @@ import requests
 import newspaper
 from bs4 import BeautifulSoup
 
+
+'''
+select vs find_all vs find
+soup.select()
+- CSS 선택자
+- 리스트(여러개)
+- 동일한 태그 동일한 id 나 class 로 다수 데이터를 가져올 때 사용
+- css style 에 작성하는 것처럼 .클래스이름 #아이디이름 표기
+soup.find_all("태그이름", class_="클래스이름"   id="아이디이름")
+- select와 완전 동일
+- css 선택자 스타일 작성이 어려워요.
+- 위와 같이 "태그이름", class or id 명칭으로 다수 데이터를 가져올 때 사용
+
+soup.find("태그이름", class_="클래스이름"   id="아이디이름")
+- 하나의 데이터를 가져올 때 사용
+- 태그 + 속성 직접 지정
+
+
+"문자열".startswith("/"):
+- ~로 시작하는지 확인하는 기능
+- "안녕하세요".startswith("안녕")  안녕하세요는 안녕으로 시작하는게 맞으므로 True
+- "안녕하세요".startswith("hi")  안녕하세요는   hi    로 시작하는게 아니므로 False
+
+크롤링을 할 때 웹 사이트 링크는 두 종류로 나뉜다.
+# 절대경로 - 주소가 완전히 다 있는 형태
+https://n.news.naver.com/article/277/0005755604
+
+
+# 상대 경로 - 앞부분이 생략됨
+/article/277/0005755604
+보통 상대경로는 앞이 https 가 아니라 /로 시작한다.
+'''
+
 headers = {
     "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 Chrome/120.0.0.0 Safari/537.36"
 }
@@ -18,7 +51,7 @@ def url목록가져오기():
     soup = BeautifulSoup(res.text,"html.parser")
 
     랭크목록 = []
-    태그들 = soup.select("a.list_title")
+    태그들 = soup.select("a.list_title") # css 선택자 findAll 처럼 여러개 갖고올 때 사용
 
     for 태그 in 태그들[:20]: #최대 20개 까지만 설정 0~19 까지
         링크 = 태그.get("href")
