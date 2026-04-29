ff3b38be04350fd1bd3cdc1731d3e3f0be475122
tjoeun-260225 <tjoeun.jr5@gmail.com>
2026-04-29 11:00:22 +0900

    데이터 csv 파일 저장

--- 07_동적크롤링/01_동적크롤링_개요.py
+++ 07_동적크롤링/01_동적크롤링_개요.py
@@ -32,16 +32,19 @@ def 셀레니움기본코드():
     driver.get("https://google.com")
     # 요소 찾기 태그 안에 있는 데이터 찾기
     search_box = driver.find_element(By.NAME, "q")
-    #텍스트 입력
+    # 텍스트 입력
     search_box.send_keys("셀레니움 이란")
     search_box.submit()
-    #결과 출력
+    # 결과 출력
     print(driver.title)
     # 브라우저 닫기 = quit()
     driver.quit()
+
+
 # 셀레니움기본코드()
 
 from playwright.sync_api import sync_playwright
+
 
 def playwright기본코드():
     # playwright시작
@@ -55,7 +58,7 @@ def playwright기본코드():
         user_agent="Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/134.0.0.0 Safari/537.36"
     )
     # 새 페이지
-    #page = browser.new_page()
+    # page = browser.new_page()
     page = context.new_page()
     page.add_init_script("Object.defineProperty(navigator, 'webdriver', {get: () => undefined})")
     # 페이지 이동
@@ -69,7 +72,61 @@ def playwright기본코드():
     # 종료
     browser.close()
     p.stop()
-playwright기본코드()
+
+
+# playwright기본코드()
+
+
+# from playwright_stealth import stealth_sync    1버전 대 사용 법
+#from playwright_stealth import Stealth  # 2버전 대 사용법
+
+
+def playwrightStealth_version2_아직은개발중인단계_코드():
+    # playwright시작
+    p = sync_playwright().start()
+    # 브라우저 열기
+    browser = p.chromium.launch(headless=False)
+    #stealth = Stealth()
+    # 새 페이지
+    page = browser.new_page()
+    #Stealth().use_sync(page).__enter__()
+    # 페이지 이동
+    page.goto("https://google.com")
+    # 요소 찾고 입력
+    page.fill('textarea[name="q"]', 'Playwright 란')
+    page.keyboard.press("Enter")
+    # 결과 기다리기
+    page.wait_for_load_state("networkidle")
+    print(page.title())
+    # 종료
+    browser.close()
+    p.stop()
+
+
+
+
+
+
+from playwright.sync_api import  sync_playwright
+
+def 구글대신네이버():
+    p = sync_playwright().start()
+    browser = p.chromium.launch(headless=False)
+    page = browser.new_page()
+
+    page.goto("https://search.naver.com/search.naver?query=강아지")
+    print(page.title())
+
+    browser.close()
+    p.stop()
+
+구글대신네이버()
+
+
+
+
+
+
 
 
 
