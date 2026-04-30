ffc0b9b556c82f928633f30d15317271050050b7
tjoeun-260225 <tjoeun.jr5@gmail.com>
2026-04-30 12:41:37 +0900

    Pandas, matplotlib

--- 08_공공데이터_데이터정제_시각화/02_맷플롯립.py
+++ 08_공공데이터_데이터정제_시각화/02_맷플롯립.py
@@ -8,20 +8,25 @@
 ####### 기본 구조
 import matplotlib.pyplot as plt
 
+# 한글 깨짐 방지 한글 폰트 세팅
+plt.rcParams['font.family'] = 'Malgun Gothic'
+plt.rcParams['axes.unicode_minus'] = False   # 마이너스 기호 깨짐 방지
+
+
 # plot = 선그래프 추세
 plt.plot([1, 2, 3], [4, 5, 6])
 plt.title("제목")
 plt.xlabel("x축")
 plt.ylabel("y축")
-
+plt.show()
 # bar  = 막대 그래프 비교
 plt.bar(["A", "B", "C"], [30, 50, 20])
 plt.title("항목별 비교")
-
+plt.show()
 # pie  = 원형 그래프 비율
-plt.pie([40, 30, 20, 10], ["A", "B", "C", "D"], autopct="%1.1f%%")
+plt.pie([40, 30, 20, 10], labels=["A", "B", "C", "D"], autopct="%1.1f%%")
 plt.title("비율 차트")
-
+plt.show()
 # scatter = 산점도 관계 확인할 때
 plt.scatter([1, 2, 3, 4], [10, 20, 15, 30])
 plt.title("두 변수의 관계")
