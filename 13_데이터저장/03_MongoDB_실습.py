b823b8de550ba11e941dfd1e6b5a1015ea224c63
tjoeun-260225 <tjoeun.jr5@gmail.com>
2026-05-08 17:04:32 +0900

    데이터베이스 / nosql / mongoDB

--- 13_데이터저장/03_MongoDB_실습.py
+++ 13_데이터저장/03_MongoDB_실습.py
@@ -55,6 +55,8 @@ def insert_문제1_도서관_책_등록():
             },
         ]
     )
+
+
 def insert_문제2_쇼핑몰_상품_등록():
     결과 = col_products.insert_many([
         {
@@ -92,6 +94,8 @@ def insert_문제2_쇼핑몰_상품_등록():
             데이터를 제대로 저장하자 xxxx -> 데이터를 빠르게 조회 / 데이터 작업을 할 때 문제 최소화
     
     '''
+
+
 def insert_문제3_SNS_게시물_등록():
     # 게시물 1 - insert_one
     한건결과 = col_posts.insert_one(
@@ -105,27 +109,126 @@ def insert_문제3_SNS_게시물_등록():
     print("게시물 1 저장 id : ", 한건결과.inserted_id)
     # 데이터 다수 = [  ] 시작   데이터 한 건 = {  }
 
-    여러건결과 =col_posts.insert_many(
+    여러건결과 = col_posts.insert_many(
         [
             {
                 "author": "이몽고",
                 "content": "맛집 발견",
-                "images": ["img1.jpg","img2.jpg","img3.jpg"],
+                "images": ["img1.jpg", "img2.jpg", "img3.jpg"],
                 "likes": 0,
-                "comments": [] ,
-                "created_at": datetime.now() ,
+                "comments": [],
+                "created_at": datetime.now(),
             },
             {
                 "author": "박클라",
                 "content": "주말 코딩중",
-                "hashtags": ["개발","파이썬","mongoDB"],
+                "hashtags": ["개발", "파이썬", "mongoDB"],
                 "likes": 5,
                 "comments": [
-                    {"writer":"최데이", "text":"멋지다!"},
-                    {"writer":"김개발", "text":"나도 공부해야지"}
-                ] ,
-                "created_at": datetime.now() ,
+                    {"writer": "최데이", "text": "멋지다!"},
+                    {"writer": "김개발", "text": "나도 공부해야지"}
+                ],
+                "created_at": datetime.now(),
             }
         ]
     )
-insert_문제3_SNS_게시물_등록()
+
+
+def read_문제1_도서관_books_조회():
+    # 1. books 전체 조회
+    # find() = select *   col_books = from books
+    #         from books   select  *
+    #          col_books  .find  (  ):
+    for doc in col_books.find():
+        print("전체 조회 : ", doc)
+
+    # 2. author 가 "김코딩" 인 책 한 건 조회
+    # find_one({"author": "김코딩"})
+    # SELECT *   FROM Books WHERE author =  "김코딩"
+    doc = col_books.find_one({"author": "김코딩"})
+    print("김코딩 한 건 조회 : ", doc)
+
+    # 3. price 가 20000 이상인 책만 조회
+    for doc in col_books.find({"price": {"$gte": 20000}}):
+        print(f"가격이 20000 이상인데이터 조회 : {doc}")
+
+    # 4. pub_year 기준 최신순(내림차순) 정렬 조회
+    for doc in col_books.find().sort("pub_year", -1):
+        print(f"최신순 정렬 : {doc}")
+
+    # 5. 가격 낮은 책 2권만 조회 (오름차순 정렬 + limit)
+    for doc in col_books.find().sort("price", 1).limit(2):
+        print(f"가격 낮은 책 2권만 조회 : {doc}")
+
+    # 6. title, price 필드만 보기 (_id 숨기기)
+    for doc in col_books.find({}, {"_id": 0, "title": 1, "price": 1}):
+        print(f"title, price 만 조회 : {doc}")
+
+    # 7. books 컬렉션 전체 책 권수 세기
+    count = col_books.count_documents({})
+    print(f"전체 책 권수 세기 : {count} ")
+
+    # 8. author 가 "홍길동" 인 책 존재 여부 확인 (True/False)
+    exists = col_books.find_one({"author": "홍길동"}) is not None
+    print(f"홍길동 존재 여부 : {exists}")
+
+
+def read_문제2_쇼핑몰_product_조회():
+    # 1. products 전체 조회
+    for doc in col_products.find():
+        print("전체 조회 : ", doc)
+    # 2. price 가 50000 이상인 상품만 조회
+    for doc in col_products.find({"price": {"$gte": 50000}}):
+        print("50000 이상 : ", doc)
+
+    # 3. price 기준 내림차순 정렬 조회
+    for doc in col_products.find().sort("price", -1):
+        print("가격 내림차순 : ", doc)
+    # 4. 가장 비싼 상품 1개만 조회
+    doc = col_products.find_one({}, sort=[("price", -1)])
+    print("가장 비싼 상품 : ", doc)
+    # 5. product_name, price 필드만 보기 (_id 숨기기)
+    for doc in col_products.find({}, {"_id": 0, "product_name": 1, "price": 1}):
+        print("product_name, price 조회 : ", doc)
+
+    # 6. stock 이 50 이하인 상품 개수 세기
+    count = col_products.count_documents({"stock": {"$lte": 50}})
+    print("stock 50 이하 총 개수 : ", count)
+    # 7. 2번째 상품부터 2개만 조회 (skip + limit)
+    for doc in col_products.find().skip(1).limit(2):
+        print("skip + limit : ", doc)
+    # 8. discount_rate 필드가 존재하는 상품만 조회
+    for doc in col_products.find({"discount_rate": {"$exists": True}}):
+        print("할인 상품 : ", doc)
+
+
+def read_문제3_SNS_posts_조회():
+    # 1. posts 전체 조회
+    for doc in col_posts.find():
+        print("전체 조회 : ", doc)
+    # 2. author 가 "김개발" 인 게시물 한 건 조회
+    doc = col_posts.find_one(({"author": "김개발"}))
+    print("김개발 한 건 조회 : ", doc)
+    # 3. likes 가 1 이상인 게시물만 조회
+    # hint : find({"likes": {"$gte": 1}})
+    for doc in col_posts.find({"likes": {"$gte": 1}}):
+        print("likes 1 이상 인 게시물 : ", doc)
+    # 4. likes 기준 내림차순 정렬 조회
+    for doc in col_posts.find().sort("likes", -1):
+        print("likes 내림차순 : ", doc)
+    # 5. author, content, likes 필드만 보기 (_id 숨기기)
+    for doc in col_posts.find({}, {"_id": 0, "author": 1, "content": 1, "likes": 1}):
+        print("author, content, likes 조회 : ", doc)
+    # 6. hashtags 필드가 존재하는 게시물만 조회
+    # hint : find({"hashtags": {"$exists": True}})
+    for doc in col_posts.find({"hashtags": {"$exists": True}}):
+        print("해시태그가 존재하는 게시물만 조회 : ", doc)
+    # 7. comments 배열 안에 writer 가 "최데이" 인 게시물 조회
+    # hint : find({"comments.writer": "최데이"})
+    for doc in col_posts.find({"comments.writer": "최데이"}):
+        print("최데이 댓글을 단 게시물만 조회", doc)
+    # 8. 전체 게시물 수 세기
+    count = col_posts.count_documents({})
+    print("전체 게시무 수 : ", count)
+
+read_문제3_SNS_posts_조회()
