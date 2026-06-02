581b078f928a8ecc5c8361f20f07c2a41beced53
tjoeun-260225 <tjoeun.jr5@gmail.com>
2026-06-02 12:21:35 +0900

    딥러닝 모델 학습 / 만들기

--- 19_Deep_Learning/06_tensorflow_dataset.py
+++ 19_Deep_Learning/06_tensorflow_dataset.py
@@ -28,7 +28,7 @@ Boston Housing    집값예측   1개            회귀 : mse
 loss 이미지 글자 예측 도 많이 사용하는 loss가 정해져 있다.
 """
 import tensorflow as tf
-from tensorflow.keras.datasets import mnist
+from tensorflow.keras.datasets import mnist, fashion_mnist, california_housing
 from tensorflow.keras import Sequential
 from tensorflow.keras.layers import Dense, Input, Flatten
 
@@ -44,12 +44,6 @@ def 손글씨_딥러닝():
     # 텐서플로우 구글 자체에서 권장하는 코드
     # 텐서플로우에서 제공하는 손글씨를 불러와 훈련용과 정답용으로 나누어 분리하기
     (X_train, y_train), (X_test, y_test) = mnist.load_data()
-
-    # 데이터 기본 정보 출력
-    print(f"훈련 이미지 수 : {X_train.shape[0]}")
-    print(f"이미지 크기 : {X_train.shape[1]} x {X_train.shape[2]}")
-    print(f"픽셀값 범위: {X_train.min()} x {X_train.max()}")
-
     """
     훈련 이미지 수 : 60000
     이미지 크기 : 28 x 28
@@ -73,17 +67,107 @@ def 손글씨_딥러닝():
 
     # 로봇 뇌 만들기
     model = Sequential([
+        # ❌❌❌❌❌❌❌❌❌❌
+        # tensorflow.keras.layers.Dense(8, activation='relu', input_shape=(1,)),
+        # tf.keras.layers.Dense(8, activation='relu', input_shape=(1,)),
+
+        # AI가 위와 같은 코드를 제공한다면 레거시한 AI 모델이다.
+
+        # 맨 위에
+        # from tensorflow.keras.layers import Dense, Input, Flatten
+        # 이 한줄만 작성하면 매번 tensorflow에서 keras에 있는 layers 내에 존재하는
+        # Dense 이다 를 매번 작성하지 않아도 된다.
+        # ⭕⭕⭕⭕⭕⭕⭕⭕⭕⭕
         Input(shape=(28, 28)),  # tensorflow 에서 28x28로 데이터 호출해서 사용하라 적힌대로 작성
         Flatten(),
         Dense(128, activation='relu'),
         Dense(10, activation='softmax')
     ])
     model.compile(optimizer='adam',
-                  loss='sparse_categorical_crossentropy',
+                  # 보통 이미지에서 loss 로 sparse_categorical_crossentropy 많이 사용
+                  # loss = 정답 채점 방식
+                  # binary              = O/X 문제 둘 중 하나 채점할 때 사용
+                  # categorical        = 정답을 배열로 표시
+                  # sparse_categorical = 정답을 숫자로 표시
+                  loss='sparse_categorical_crossentropy',  # 강아지 고양이 개 1,2,3 나누어서 정답인 숫자 표현
                   metrics=['accuracy'])
-    model.fit(X_train, y_train, epochs=10, verbose=1)
+    model.fit(X_train, y_train, epochs=10, verbose=1, batch_size=32)
+
+    """
+    손글씨 훈련 데이터 : 60000 장
+    epochs = 10               60,000 장 짜리 데이터를 처음부터 끝까지 읽는 것을 10번 반복
+    수능EBS 수학책을 10회독 하는 것과 똑같은 것
+    
+    10회독을 할 때
+    1회독 책 1권 160장 월요일 30장 화요일 30장 수요일 30장 목요일 30장 금요일 30장 토요일 10장
+    ...
+    총 10번 한다
+    마지막에는 60000장 / 배치사이즈 32 = 1875 나뉜 나머지 313 장 공부하겠다.
+    
+    1875/1875
+    313/313 
+    """
+
     loss, acc = model.evaluate(X_test, y_test, verbose=1)
     print(f"[MNIST]정확도 : {acc * 100:.1f}%")
 
 
-손글씨_딥러닝()
+# 2. 옷 맞추기
+def run_fashion_mnist():
+    # Step 1. 데이터 불러오기
+    (X_train, y_train), (X_test, y_test) = fashion_mnist.load_data()
+
+    # Step 2. 정규화 - 픽셀값 0~255를 0~1로 바꾸기
+    X_train, X_test = X_train / 255.0, X_test / 255.0
+
+    # Step 3. 모델 설계
+    model = Sequential([
+        Input(shape=(28, 28)),
+        Flatten(),
+        # 보통 Dense 에서 숫자값은
+        # 입출력층을 제외하고
+        # 8 16 32 64 128 256 512 단위 형태로 많이 사용
+        # 128 256 64 많이 사용
+        Dense(256, activation='relu'),
+        Dense(10, activation='softmax')
+    ])
+
+    # Step 4. 컴파일
+    model.compile(
+        optimizer='adam',  # 99.9 adam 사용
+        loss='sparse_categorical_crossentropy',  # 이미지
+        metrics=['accuracy']  # 확률 쓰지 않는 한 변하지 않는다.
+    )
+
+    # Step 5. 학습
+    # 학습쪽에 epochs 나 batch_size 조절해서 정답률 높이기
+    model.fit(X_train, y_train, epochs=30, verbose=1)
+
+    # Step 6. 평가
+    loss, acc = model.evaluate(X_test, y_test, verbose=0)
+    print(f"[Fashion MNIST] 정확도: {acc * 100:.1f}%")
+
+
+# 3. 집값 예측하기 - 윤리적인 문제로 인하여 sklearn 과 마찬가지로 사용 자제
+def 캘리포니아집값예측():
+    (X_train, y_train), (X_test, y_test) = california_housing.load_data()
+
+    # sklearn 에서 했던 정규화 작업을 추가할 수 있다.
+    # 방 개수 : 1 ~ 10            -3 ~ 3
+    # 인구 수 : 100 ~ 35,000      -3 ~ 3
+    # 집   값 : 0.1 ~ 5.0         -3 ~ 3
+    # 각각의 최대최소를 이용하여 범위를 동일하게 맞추어 주는 것이 좋다.
+    # 범위가 너~무 달라서 정규화가 없으면 Loss 크게 나온다.
+    # StandardScaler 와 같은 기능을 이용해서  들쑥 날쑥 한 범위를 각 컬럼 속성에 맞게 통일
+
+    model = Sequential([
+        Input(shape=(8, 0)),  # tensorflow Docs에 8개의 컬럼으로 이루어져 있다 쓰여있다.
+        Dense(64, activation='relu'),
+        Dense(32, activation='relu'),
+        Dense(1)
+    ])
+
+    model.compile(optimizer='adam', loss='mse')
+    model.fit(X_train, y_train, epochs=50, verbose=1)
+    loss = model.evaluate(X_test, y_test, verbose=1)
+    print(f"캘리포니아 집값 예측 : {loss:.2f}%")
